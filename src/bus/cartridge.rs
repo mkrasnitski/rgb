623f7164394707139e073563aabf1ca095d5dd7b
Michael Krasnitski <michael.krasnitski@gmail.com>
2024-01-23 00:40:45 -0500

    Full MBC1 support

--- src/bus/cartridge.rs
+++ src/bus/cartridge.rs
@@ -18,14 +18,40 @@ impl Mapper for NoMapper {
 struct MBC1 {
     rom: Vec<u8>,
     num_banks: u16,
-    bank: u16,
+    bank1: u8,
+    bank2: u8,
+    mode: bool,
+}
+
+impl MBC1 {
+    pub fn new(rom: Vec<u8>, num_banks: u16) -> Self {
+        Self {
+            rom,
+            num_banks,
+            bank1: 1,
+            bank2: 0,
+            mode: false,
+        }
+    }
+
+    fn lo_bank(&self) -> usize {
+        if self.mode {
+            (self.bank2 << 5) as usize % self.num_banks as usize
+        } else {
+            0
+        }
+    }
+
+    fn hi_bank(&self) -> usize {
+        ((self.bank2 << 5) | self.bank1) as usize % self.num_banks as usize
+    }
 }
 
 impl Mapper for MBC1 {
     fn read(&self, addr: u16) -> u8 {
         match addr {
-            0x0000..=0x3fff => self.rom[addr as usize],
-            0x4000..=0x7fff => self.rom[self.bank as usize * 0x4000 + addr as usize - 0x4000],
+            0x0000..=0x3fff => self.rom[self.lo_bank() * 0x4000 + addr as usize],
+            0x4000..=0x7fff => self.rom[self.hi_bank() * 0x4000 + addr as usize - 0x4000],
             _ => unreachable!(),
         }
     }
@@ -33,13 +59,72 @@ impl Mapper for MBC1 {
     fn write(&mut self, addr: u16, val: u8) {
         match addr {
             0x2000..=0x3fff => {
-                self.bank = if val == 0 {
-                    1
+                let val = val & 0x1f;
+                self.bank1 = if val != 0 {
+                    val % self.num_banks as u8
                 } else {
-                    (val as u16 & 0x1f) % self.num_banks
+                    1
+                };
+            }
+            0x4000..=0x5fff => {
+                self.bank2 = val & 0b11;
+            }
+            0x6000..=0x7fff => self.mode = val != 0,
+            _ => panic!("MBC1 write: ${addr:04x} = {val:02x}"),
+        }
+    }
+}
+
+struct MBC1Ram {
+    mbc1: MBC1,
+    ram: Vec<u8>,
+    ram_bank: u8,
+    ram_enabled: bool,
+}
+
+impl MBC1Ram {
+    fn ram_bank(&self) -> usize {
+        if self.mbc1.mode {
+            self.ram_bank as usize
+        } else {
+            0
+        }
+    }
+}
+
+impl Mapper for MBC1Ram {
+    fn read(&self, addr: u16) -> u8 {
+        match addr {
+            0x0000..=0x7fff => self.mbc1.read(addr),
+            0xa000..=0xbfff => {
+                if self.ram_enabled {
+                    self.ram[self.ram_bank() * 0x2000 + addr as usize - 0xa000]
+                } else {
+                    0xFF
                 }
             }
-            _ => panic!("MBC1 write: ${addr:04x} = {val:02x}"),
+            _ => unreachable!(),
+        }
+    }
+
+    fn write(&mut self, addr: u16, val: u8) {
+        match addr {
+            0x0000..=0x1fff => self.ram_enabled = (val & 0xf) == 0xA,
+            0x2000..=0x3fff | 0x6000..=0x7fff => self.mbc1.write(addr, val),
+            0x4000..=0x5fff => {
+                self.mbc1.write(addr, val);
+                if self.mbc1.mode {
+                    let num_ram_banks = ((self.ram.len() + 0x1fff) / 0x2000) as u8;
+                    self.ram_bank = (val & 0b11) % num_ram_banks;
+                }
+            }
+            0xa000..=0xbfff => {
+                if self.ram_enabled {
+                    let ram_addr = self.ram_bank() * 0x2000 + addr as usize - 0xa000;
+                    self.ram[ram_addr] = val
+                }
+            }
+            _ => panic!("MBC1+Ram write: ${addr:04x} = {val:02x}"),
         }
     }
 }
@@ -51,18 +136,33 @@ pub struct Cartridge {
 impl Cartridge {
     pub fn new(rom: Vec<u8>) -> Self {
         let mbc = rom[0x147];
-        let rom_size = rom[0x148];
+        let rom_type = rom[0x148];
+        let ram_type = rom[0x149];
+
+        let num_banks = match rom_type {
+            0x00..=0x08 => 2 << rom_type,
+            _ => unreachable!(),
+        };
+        let ram_size_kb = match ram_type {
+            0x00 => 0,
+            0x01 => 2,
+            0x02 => 8,
+            0x03 => 32,
+            0x04 => 128,
+            0x05 => 16,
+            _ => unreachable!(),
+        };
+
         let mapper: Box<dyn Mapper> = match mbc {
             0x00 => Box::new(NoMapper {
                 rom: rom.try_into().unwrap(),
             }),
-            0x01 => Box::new(MBC1 {
-                rom,
-                num_banks: match rom_size {
-                    0x00..=0x08 => 2 << rom_size,
-                    _ => unreachable!(),
-                },
-                bank: 1,
+            0x01 => Box::new(MBC1::new(rom, num_banks)),
+            0x02 | 0x03 => Box::new(MBC1Ram {
+                mbc1: MBC1::new(rom, num_banks),
+                ram: vec![0; 1024 * ram_size_kb as usize],
+                ram_bank: 0,
+                ram_enabled: false,
             }),
             _ => panic!("Invalid mapper value: {mbc:02x}"),
         };
@@ -70,16 +170,10 @@ impl Cartridge {
     }
 
     pub fn read(&self, addr: u16) -> u8 {
-        match addr {
-            0..=0x7fff => self.mapper.read(addr),
-            0x8000..=0xffff => unreachable!(),
-        }
+        self.mapper.read(addr)
     }
 
     pub fn write(&mut self, addr: u16, val: u8) {
-        match addr {
-            0..=0x7fff => self.mapper.write(addr, val),
-            0x8000..=0xffff => unreachable!(),
-        }
+        self.mapper.write(addr, val);
     }
 }
